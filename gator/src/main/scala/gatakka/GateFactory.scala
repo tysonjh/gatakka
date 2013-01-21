de488e22aa815d417f78e990d2df346acbf14a4e
Tyson <thamilton@verticalscope.com>
2013-01-21 17:04:48 -0500

    Added lock to elminate race conditions between actors for calling Gate.init

--- gator/src/main/scala/gatakka/GateFactory.scala
+++ gator/src/main/scala/gatakka/GateFactory.scala
@@ -2,18 +2,22 @@ package gatakka
 
 import gate.Gate
 import java.io.File
+import concurrent.Lock
 
 object GateFactory {
   private [gatakka] val pluginsDirectory = "/plugins"
+  private [gatakka] val lock = new Lock
 
   /**
    * Gate must be initialized exactly once.
    */
-  def init {
+  def init() {
+    lock.acquire()
     if (!Gate.isInitialised){
       Gate.runInSandbox(true)
       Gate.init()
     }
+    lock.release()
   }
 
 }
